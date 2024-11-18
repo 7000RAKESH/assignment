45b0e84192c9626b78c3dc9324ba6e1e52c6b885
7000RAKESH <rakeshmca222@gmail.com>
2024-11-18 09:49:59 +0530

    yes

---
+++ assignment4.js
@@ -0,0 +1,36 @@
+// /DATE : 13-11-24
+
+// 1
+// arr = [30000, 40000, 50000, 35000];
+// const salary = arr.map((value) => value + value * 0.1);
+// console.log(salary);
+
+// 2
+// let array = [23, 45, 60, 70, 33];
+// let array2 = [];
+// const odd = array.map((value) => {
+//   if (value % 2 != 0) {
+//     array2.push(value);
+//   }
+// });
+// console.log(array2);
+
+// 3
+// let array = ["soap", "shampoo", "toothpaste"];
+// const array2 = array.map((value) => value.toLocaleUpperCase());
+// console.log(array2);
+
+// 4
+// let score = [55, 70, 82, 47];
+// const passed = score.filter((val) => {
+//   if (val > 50) {
+//     pass.push(val);
+//     console.log(val);
+//   }
+// });
+
+// 5
+// let names = [{ name: "Alice" }, { name: "Bob" }, { name: "Charlie" }];
+// const values = names.map((val) => {
+//   console.log(val.name);
+// });
