daf2851d3c47e73138dd6930eea304d497f8c55f
7000RAKESH <rakeshmca222@gmail.com>
2024-11-18 10:32:44 +0530

    yes

---
+++ assignment5.js
@@ -0,0 +1,104 @@
+// DATE : 14-11-24
+
+// 1
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// for (val in person) {
+//   console.log(`${val} : ${person[val]}`);
+// }
+
+// 2
+// let numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for (val of numbers) {
+//   sum += val;
+// }
+// console.log(sum);
+
+// 3
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// Object.freeze(person);
+// person.age = 30;
+// console.log(person);
+
+// 4
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// Object.seal(person);
+// person.age = 30;
+// person.salary = 100000;
+// console.log(person);
+
+// 5
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// let output = Object.keys(person);
+// console.log(output);
+
+// 6
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// let output = Object.values(person);
+// console.log(output);
+
+// 7
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// let output = Object.entries(person);
+// console.log(output);
+
+// 8
+// const student = {
+//   name: "rakesh",
+//   subjects: {
+//     maths: 90,
+//     english: 90,
+//   },
+// };
+
+// for (val in student) {
+//   console.log(`${val} : ${student[val]}`);
+//   console.log(student[val]);
+// }
+
+// 9
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// for (val in person) {
+//   console.log(`${val} : ${person[val]}`);
+// }
+// let output = Object.values(person).forEach((value) => console.log(value));
+// console.log(output);
+
+// 10
+// const person = {
+//   name: "rakesh",
+//   age: 22,
+//   country: "INDIA",
+// };
+// Object.freeze(person);
+// person.age = 90;
+// person.salary = 78389;
+// console.log(person);
